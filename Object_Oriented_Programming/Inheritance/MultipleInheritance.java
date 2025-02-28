e975cbd4f0f3ed55b10d0c2fe33fc19c8df95219
harish-bendale <harishbendale80@gmail.com>
2025-02-28 09:13:28 +0530

    Code added

---
+++ Object_Oriented_Programming/Inheritance/MultipleInheritance.java
@@ -0,0 +1,31 @@
+package Object_Oriented_Programming.Inheritance;
+interface Shape {
+    int area();
+}
+
+interface Output {
+    void print(int a);
+}
+
+class Rectangle implements Shape, Output {
+    int length , breadth;
+
+    Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+
+    public void print(int a) {
+        System.out.println(a);
+    }
+}
+public class MultipleInheritance {
+    public static void main(String[] a) {
+        Rectangle r = new Rectangle(10, 2);
+        r.print(r.area());
+    }
+}
